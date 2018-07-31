7358a7abfcd92e4dd26714f781075229f5df2175
AliceEmer <aecourte21@gmail.com>
2018-07-31 15:13:42 +0200

    CORS middleware added

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/AliceEmer/API-IRIS/controllers"
+	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris"
 
 	"github.com/go-pg/pg"
@@ -19,13 +20,19 @@ func main() {
 
 	cn.DB = db
 
-	app := iris.Default()
+	app := iris.New()
 
-	app.Post("/signup", cn.SignUp)
-	app.Post("/signin", cn.SignIn)
+	crs := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
+		AllowCredentials: true,
+		AllowedMethods:   []string{"Get", "Post", "Delete", "PUT"},
+	})
+
+	app.Post("/signup", cn.SignUp, crs)
+	app.Post("/signin", cn.SignIn, crs)
 
 	//Routing group
-	api := app.Party("/api", apiMiddleware)
+	api := app.Party("/api", apiMiddleware, crs)
 
 	api.Get("/persons", cn.GetAllPersons)
 	api.Get("/persons/{id:int}", cn.GetPersonByID)
@@ -39,7 +46,6 @@ func main() {
 
 //No access to api/ if the JWT is not created or not valid
 func apiMiddleware(c iris.Context) {
-	// [...]
 	if cn.CheckJWT(c) {
 		c.Next()
 	}
